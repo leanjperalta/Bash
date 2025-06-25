d56f84d8bcb34588b2452ce701b37a2bb2abfe8e
Leandro Peralta <90806231+leanjperalta@users.noreply.github.com>
2025-06-25 10:46:50 -0300

    Update dns_nic.sh
    Updated with mail alert.

--- dns_nic.sh
+++ dns_nic.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# This script checks if the nameservers for the domain "XXXXX" are correctly delegated.
-# It queries the DNS server at NIC 
+# This script checks if the nameservers for the domain "garrahan.gov.ar" are correctly delegated.
+# It queries the DNS server at NIC Argentina and uses `dig` to check for the presence of the nameserver "ns1.garrahan.gov.ar".
+# In case of failure, it sends an email alert to the specified address. A proper .mailrc file is required for email functionality.
 
-domain_name="xxxxxx"
+domain_name="xxxxxxx"
 dns=1.1.1.1
+mail_to="lperalta@xxxxxxx"
 
 for list in $(dig @"$dns" "$domain_name"); do
-    if [[ "$list" == *"ns1.xxxxxxxx"* ]]; then #modify with delegated server/s for the domain
+    if [[ "$list" == *"ns1.xxxxxxx"* ]]; then #Modify with delegated nameserver for domain
         echo "Found nameserver/s: $list"
         found=1
     fi
@@ -15,8 +17,10 @@ done
 
 if [[ -z "$found" ]]; then
     
-        echo "Delegated nameservers not found for $domain_name"
+        echo "Delegated nameservers not found for $domain_name" | s-nail -s "DNS Alert" $mail_to
+
       
 fi
+
 
 
