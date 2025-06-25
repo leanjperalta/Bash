7485d17aef4edbe64508b7b0423fce156f10dee2
Leandro Peralta <90806231+leanjperalta@users.noreply.github.com>
2025-06-25 10:27:32 -0300

    Update dns_nic.sh

--- dns_nic.sh
+++ dns_nic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# This script checks if the nameservers for the domain "garrahan.gov.ar" are correctly delegated.
-# It queries the DNS server at NIC Argentina
+# This script checks if the nameservers for the domain "XXXXX" are correctly delegated.
+# It queries the DNS server at NIC 
 
-domain_name="garrahan.gov.ar"
+domain_name="xxxxxx"
 dns=1.1.1.1
 
 for list in $(dig @"$dns" "$domain_name"); do
-    if [[ "$list" == *"ns1.garrahan.gov.ar"* ]]; then
+    if [[ "$list" == *"ns1.xxxxxxxx"* ]]; then #modify with delegated server/s for the domain
         echo "Found nameserver/s: $list"
         found=1
     fi
